3df8ce279a75eac743b1a973aa125e3f91e8099b
turkas64 <turkas64@gmail.com>
2015-08-13 20:42:19 +0300

    BOŞ SORU EKLENME SORUNU
    Soru veya cevaplardan biri dahi boşsa eklenmez!!!

--- admin/add-question.php
+++ admin/add-question.php
@@ -5,14 +5,15 @@
 if($_POST){
     require("../inc/functions.php");
     autoLoader();
-    $questions = getAllQuestions("../");
+    $questions = getAllQuestions();
     // veriyi yazacağımız dosya yolu
     $questionsDataFilePath = "../data/questions.txt";
     $message = "Soru eklenemedi";
 
     // eski sorulara yenisini ekliyoruz
-    array_push($questions, $_POST);
-
+    if((!empty($_POST["question"]))&&(!empty($_POST["options['a']"]))&&(!empty($_POST["options['b']"]))&&(!empty($_POST["options['c']"]))&&(!empty($_POST["options['d']"]))){
+        array_push($questions, $_POST);
+    }
     // yeni soru eklenmiş haliyle sorular değişkeni
     $encodedQuestions = json_encode($questions);
 
@@ -31,15 +32,16 @@ if($_POST){
 <?php
 if(isset($message)) echo $message."<hr>";
 ?>
-<form action="" method="post" class="form-group">
-    Soru: <input type="text" name="question"><br>
+<form action="" method="post" class="questions"><br>
+    SORU : <input type="text" name="question"><br>
     <?php
+
     for($i="a"; $i<="d"; $i++):
         ?>
-        <?=($i)?> <input type="text" name="options[<?=$i?>]">&nbsp;<input type="radio" name="answer" value="<?=$i;?>" <?if($i=="a") echo "checked"; ?>><br>
+        <?=($i)?> <input type="text" name="options[<?=$i?>]">&nbsp;<input type="radio" name="answer" value="<?=$i;?>" <?if($i=="a") echo "checked"; ?><br>
     <?php
     endfor;
     ?>
-    <input type="submit" value="Soru Ekle">
+    <input type="submit" name="ekle" value="Soru Ekle">
 </form>
 <?php include("../inc/footer.php"); ?>
